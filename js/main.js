ed0e3c825a48243c78a283bb82fced0bb6b0d2e1
isasalmeron <isabelasalmeron@msn.com>
2017-05-19 22:52:03 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,10 +6,15 @@ function init() {
 
     camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
 	camera.position.x = 0;
-	camera.position.y = 0;
-    camera.position.z = 0;
+	camera.position.y = 100;
+    camera.position.z = 600;
 
     scene = new THREE.Scene();
+    
+	// luz pra fazer o dado aparecer
+	var luz = new THREE.DirectionalLight(0xffffff);
+	luz.position.set(0, 1, 1);
+	scene.add(luz);
 
 	// carrega o modelo
 	var loader = new THREE.OBJLoader();
@@ -17,9 +22,14 @@ function init() {
 		scene.add(object);
 
 	});
+	
+	render();
 
 }
 
 function render() {
+
+	camera.lookAt(scene.position);
+	renderer.render(scene, camera);
 
 }
