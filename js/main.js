5b889d34edce1cafab70a6fb6c07a6a65f52a5d0
isasalmeron <luciane.silva@msn.com>
2017-06-02 19:56:47 -0300

    sonic

--- js/main.js
+++ js/main.js
@@ -1,37 +1,63 @@
-var camera, scene, renderer;
+var camera, scene, renderer, controls;
+var mtlLoader, objLoader;
+var rings, sonic, clouds, sun;
 
 init();
 animate();
 
 function init() {
 
-    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+    camera = new THREE.PerspectiveCamera(8, window.innerWidth / window.innerHeight, 1, 1000);
 	camera.position.x = 0;
 	camera.position.y = 100;
-    camera.position.z = 600;
+    camera.position.z = 500;
 	
     scene = new THREE.Scene();
-    
-	// luz pra fazer o dado aparecer
-	var luz = new THREE.DirectionalLight(0xffffff);
-	luz.position.set(0, 1, 1);
-	scene.add(luz);
+	rings = new Array();
+	clouds = new Array();
 
-	// carrega o modelo
-	var loader = new THREE.OBJLoader();
-	loader.load('obj/Dice.obj', function (object) {
-		scene.add(object);
-
-	});
-        
-    renderer = new THREE.WebGLRenderer();
+	renderer = new THREE.WebGLRenderer({ alpha: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    window.addEventListener('resize', onWindowResize, false);
+	// adiciona luz ambiente
+	var ambientLight = new THREE.AmbientLight(0x404040, 4.8);
+	scene.add(ambientLight);
 
+	// faz a camera mexer
+	controls = new THREE.OrbitControls(camera, renderer.domElement);
+	controls.addEventListener('change', render);
+
+	// so permite mexer a camera horizontalmente
+	controls.minPolarAngle = Math.PI/2;
+	controls.maxPolarAngle = 0;
+	controls.minAzimuthAngle = - Infinity;
+	controls.maxAzimuthAngle = Infinity;
+
+	// carrega o sonic
+	mtlLoader = new THREE.MTLLoader();
+	mtlLoader.setPath('obj/sonic/');
+	mtlLoader.load('Sonic.mtl', function(materials) {
+
+		materials.preload();
+
+		objLoader = new THREE.OBJLoader();
+		objLoader.setMaterials(materials);
+		objLoader.setPath('obj/sonic/');
+		objLoader.load('Sonic.obj', function (object) {
+			sonic = object;
+        	sonic.scale.set( 1, 1, 1 );
+        	sonic.rotateY(Math.PI/1.6);
+        	sonic.position.set(-50, -35, 0);
+
+			scene.add(object);
+		});
+	});
+
+    window.addEventListener('resize', onWindowResize, false);
 }
+
 
 function onWindowResize() {
 
@@ -44,8 +70,11 @@ function onWindowResize() {
 
 function animate() {
 
-    requestAnimationFrame(animate);
-    render();
+	requestAnimationFrame(animate);
+
+	controls.update();
+
+	render();
 
 }
 
